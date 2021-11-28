8e7161978758c738f18c2464d9d3ddb7d235d89b
Ninroot <averyformalname@gmail.com>
2021-11-28 10:29:11 +0800

    added postgres database

--- schedjuice4/settings.py
+++ schedjuice4/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'schedjuice4.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config("DBNAME"),
+        'USER': config("DBUSER"),
+        'PASSWORD': config("DBPASS"),
+        'HOST': config("DBHOST"),
+        'PORT': 5432,
+
     }
 }
 
