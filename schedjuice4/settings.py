7b4a28b92d85e1f1f79269f6745c037a6fb52bf2
Ninroot <averyformalname@gmail.com>
2022-01-01 22:09:29 +0800

    static

--- schedjuice4/settings.py
+++ schedjuice4/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(os.getcwd(),"static")
 # Media files
 
 MEDIA_URL = '/media2/'
