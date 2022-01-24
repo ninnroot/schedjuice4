0ca5d7420bfa2c0e962a8f9f8c2b73ed9e250f12
Ninroot <averyformalname@gmail.com>
2022-01-24 20:31:56 +0800

    keys exposed, so, now fixed

--- schedjuice4/settings.py
+++ schedjuice4/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w=mhlz%5h7*%47irg#yvts$=yihs$)!7)0t0=jbz*!z#)h-if^'
+SECRET_KEY = config("DJANGO")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG",default=False,cast=bool)
